c5969770ef5782bda0925685805b787bdedd13ac
Lucas Proença <siqueira12lukinha@gmail.com>
2023-11-13 14:53:06 -0300

    Atualização dos Stored Procedure

--- BANCO DE DADOS/Stored Procedure.sql
+++ BANCO DE DADOS/Stored Procedure.sql
@@ -12,3 +12,64 @@ BEGIN
     INSERT INTO Cadastro_Cliente (id_Cliente, Nome_Cliente, CPF_Cliente, Data_NascCliente, Telefone, Email, senha_Cliente)
     VALUES (@idCliente, @NomeCliente, @CPFcliente, @DataCliente, @Telefone, @Email, @senhaCliente);
 end;
+
+create procedure InserirMesa
+    @IdMesa char (2),
+    @QuantiMesa varchar (6),
+    @StatusMesa varchar (20)
+as
+begin
+    insert into Mesa (id_Mesa, Quant_Pessoas, StatusMesa)
+    values (@IdMesa, @QuantiMesa, @StatusMesa);
+END;
+
+create procedure InserirReserva
+    @idReserva char (5),
+    @dataReserva date,
+    @horaInicio time,
+    @horaFim time,
+    @precoReserva float (4),
+    @idMesa char (2),
+    @idCliente char (5)
+as
+begin
+    insert into Reserva (id_Reserva, Data_Reserva, Hora_Inicio, Hora_Fim, Preco_Reserva, id_Mesa, id_Reserva)
+    values (@idReserva, @dataReserva, @horaInicio, @horaFim, @precoReserva, @idMesa, @idCliente);
+end;
+
+create procedure InserirPedido
+    @idPedido char (5),
+    @idMesa char (2),
+    @idCliente char(5)
+as
+begin
+    insert into Pedido (id_Pedido, id_Mesa, id_Cliente)
+    values (@idPedido, @idMesa, @idCliente);
+end;
+
+create procedure InserirPrato
+    @idPrato char (5),
+    @idIngrediente char (5),
+    @precoPrato float (5),
+    @NomePrato varchar (20),
+    @idPedido char (5),
+    @idReserva char (3)
+as
+begin
+    insert into Prato (id_Prato, id_Ingrediente, Preco_Prato, Nome_Prato, id_Pedido, id_Reserva)
+    values (@idPrato, @idIngrediente, @precoPrato, @NomePrato, @idPedido, @idReserva);
+end;
+
+create procedure InserirIngrediente
+    @idIngrediente char (5),
+    @precoIngredinete float (6),
+    @dataFabricacao date,
+    @dataVencimento date,
+    @descricao varchar (150),
+    @idFornecedor char (5)
+as
+begin
+ insert into Ingrediente (id_Ingrediente, Preco_Ingrediente, Data_Fabricacao, Data_Vencimento, Descricao, id_Fornecedor)
+ values (@idIngrediente, @precoIngredinete, @dataFabricacao, @dataVencimento, @descricao, @idFornecedor);
+
+end;
